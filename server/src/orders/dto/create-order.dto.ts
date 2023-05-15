e31cc9cf91a48c12473df260853e760c0d3add97
odbayar <odbayardorjsuren8@gmail.com>
2023-05-15 18:49:36 +0800

    orders

---
+++ server/src/orders/dto/create-order.dto.ts
@@ -0,0 +1,12 @@
+export class CreateOrderDto {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  streetAddress: string;
+  city: string;
+  region: string;
+  userId: string;
+  productId: string;
+  status: boolean;
+}
